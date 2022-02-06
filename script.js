7f1e64289e9239f88282f986ac0db089f8fe2311
Bertmoss <b3rtm0ss@gmail.com>
2022-02-06 16:25:53 +0100

    moved files from the original library project repository due to fatal error/corrupt file in the original

---
+++ script.js
@@ -0,0 +1,42 @@
+const inputs = document.querySelectorAll("input");
+const submitBtn = document.querySelector("#submitBtn");
+
+//array filled with library books
+const library = [];
+
+//constructor function for new Book Objects
+function Book(
+  title,
+  author,
+  genre,
+  pages,
+  read,
+  dateStart,
+  dateEnd,
+  rating,
+  review
+) {
+  this.title = title;
+  this.author = author;
+  this.genre = genre;
+  this.pages = pages;
+  this.read = read;
+  this.dateStart = dateStart;
+  this.dateEnd = dateEnd;
+  this.rating = rating;
+  this.review = review;
+}
+
+//function prompting for library books from user
+function addBookToLibrary() {
+  let book = new Book(undefined, undefined, undefined, undefined, undefined);
+  inputs.forEach((input) => {
+    switch (input) {
+      case input.name == title:
+        book.title = input.value;
+        break;
+    }
+  });
+  return library.push(book);
+}
+console.log(library.length);
