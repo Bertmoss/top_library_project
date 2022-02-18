7480faa90077cd6736f95103a372d06fe1c824ce
Bertmoss <b3rtm0ss@gmail.com>
2022-02-18 21:03:07 +0100

    changed the layout. Made the form into a modal

--- script.js
+++ script.js
@@ -3,10 +3,8 @@
 const starRating = document.querySelectorAll(".star-rating");
 
 starRating.forEach((star) => {
-  let selectedStarValue;
   star.addEventListener("click", function () {
-    selectedStarValue = this.value;
-    activateStars(selectedStarValue);
+    activateStars(this.value);
   });
 });
 
@@ -22,17 +20,31 @@ function activateStars(selectedStarValue) {
   });
 }
 
-//HAVE YOU FINISHED IT CHECKBOX and disabling/enabling the not displayed section
-let readCheckbox = document.querySelector("#read");
+//Reset Star Images from gold to black hide bottom function
+function resetStars() {
+  const stars = document.querySelectorAll("[type = radio]");
+  stars.forEach((star) => {
+    star.checked = false;
+    star.classList.replace("golden-star", "black-star");
+  })
+}
+
+
+
+//CHECKBOX FUNCTIONALITY (HAVE YOU FINISHED IT)
+//Disabling/enabling the not displayed section
+//NEED TO ORGANIZE THE DISABLING ENABLING BETTER. SEE IF NECESSARY TO DISABLE THINGS WHEN HIDDEN
+
+
+
+const readCheckbox = document.querySelector("#read");
 
 readCheckbox.addEventListener("click", () => {
   let notDisplayedSection = document.querySelector("section:last-of-type");
   let disabledInputs = document.querySelectorAll(".disabled");
   notDisplayedSection.classList.toggle("not-displayed");
+  resetStars();
   disabledInputs.forEach((disabledInput) => {
-    //reseting the rating radio input
-    disabledInput.checked = false;
-    disabledInput.classList.replace("golden-star", "black-star");
     //reseting the values of the other inputs (excluding radio)
     if (disabledInput.getAttribute("type") !== "radio") {
       disabledInput.value = null;
@@ -43,6 +55,36 @@ readCheckbox.addEventListener("click", () => {
       : disabledInput.removeAttribute("disabled");
   });
 });
+
+const addBookBtn = document.querySelector("#add-book-btn");
+const form = document.querySelector("form");
+let notDisplayedSection = document.querySelector("section:last-of-type");
+
+
+addBookBtn.addEventListener("click", function() {
+  form.classList.remove("not-displayed");
+
+})
+
+const exitFormBtn = document.querySelector("#exit-form-btn")
+exitFormBtn.addEventListener("click", () => {
+  resetStars();
+  notDisplayedSection.classList.toggle("not-displayed");
+  form.classList.add("not-displayed");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const inputs = document.querySelectorAll("input");
 const submitBtn = document.querySelector("#submitBtn");
